cfb902d36d34cd552c261980e1f9107c47b0e255
Andrea Crotti <andrea.crotti.0@gmail.com>
2012-12-07 11:09:52 +0000

    add a Grid object

--- square_words.py
+++ square_words.py
@@ -1,10 +1,22 @@
+__metaclass__ = type
+import string
+
 # might be a nice javascript application to test out the various possibilities
 GRID_SIZE = 8
 
 DICT_FILE = 'cracklib-small'
 # if the dictionary is not too small it can be simply loaded up-front
 DICT = set(x.strip() for x in open(DICT_FILE))
+CHARS = string.ascii_uppercase
 
 
 def is_valid_word(word):
     return word in DICT
+
+
+class Grid:
+    def __init__(self):
+        pass
+
+    def count_chars(self):
+        return 0
--- test_square_words.py
+++ test_square_words.py
@@ -7,3 +7,7 @@ class TestSquareWords(unittest.TestCase):
     def test_is_valid_word(self):
         self.assertTrue(square.is_valid_word("valid"))
         self.assertTrue(not square.is_valid_word("ndfkjlk"))
+
+    def test_count_chars_empty_grid(self):
+        grid = square.Grid()
+        self.assertEqual(grid.count_chars(), 0)
