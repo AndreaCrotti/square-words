3958b25689db1b799618b279a4f6464e431feeba
Andrea Crotti <andrea.crotti.0@gmail.com>
2012-12-16 19:48:52 +0000

    refactor all the grid creation in a setUp method, and update the failing methods

--- test_square_words.py
+++ test_square_words.py
@@ -4,35 +4,33 @@ import square_words as square
 
 
 class TestSquareWords(unittest.TestCase):
+    def setUp(self):
+        self.grid = square.Grid()
+
     def test_count_chars_empty_grid(self):
-        grid = square.Grid()
-        self.assertEqual(grid.tot_chars, 0)
-        self.assertEqual(grid.empty, 64)
+        self.assertEqual(self.grid.tot_chars, 0)
+        self.assertEqual(self.grid.empty, 64)
 
     def test_count_chars_not_empty_grid(self):
-        grid = square.Grid()
-        grid[0][1] = 'a'
-        self.assertEqual(grid.tot_chars, 1)
-        self.assertEqual(grid.empty, 63)
+        self.grid[0][1] = 'a'
+        self.assertEqual(self.grid.tot_chars, 1)
+        self.assertEqual(self.grid.empty, 63)
 
     def test_set_value_in_grid_line_only_modifies_cell(self):
-        grid = square.Grid()
-        grid[0][0] = 'a'
-        self.assertEqual(grid[0][1], square.EMPTY)
-        self.assertEqual(grid[1][0], square.EMPTY)
+        self.grid[0][0] = 'a'
+        self.assertEqual(self.grid[0][1], square.EMPTY)
+        self.assertEqual(self.grid[1][0], square.EMPTY)
 
     def test_grid_with_values(self):
-        grid = square.Grid()
-        grid[0][0] = 'a'
-        self.assertEqual(grid.tot_chars, 1)
+        self.grid[0][0] = 'a'
+        self.assertEqual(self.grid.tot_chars, 1)
 
     def test_lines_columns(self):
-        grid = square.Grid()
-        grid[0][0] = 'a'
-        grid[0][1] = 'b'
-        first_line = list(grid.lines())[0]
+        self.grid[0][0] = 'a'
+        self.grid[0][1] = 'b'
+        first_line = list(self.grid.lines())[0]
         self.assertEqual(first_line[1], 'b')
-        first_col = list(grid.columns())[0]
+        first_col = list(self.grid.columns())[0]
         self.assertEqual(first_col[1], square.EMPTY)
 
     def test_words_in_line(self):
@@ -44,42 +42,36 @@ class TestSquareWords(unittest.TestCase):
         self.assertEqual(square.words_in_line(line_two), ['ac'])
 
     def test_words_in_grid(self):
-        grid = square.Grid()
-        grid[0][0] = 'a'
-        grid[0][1] = 'b'
-        grid[1][0] = 'c'
+        self.grid[0][0] = 'a'
+        self.grid[0][1] = 'b'
+        self.grid[1][0] = 'c'
         desired = ['ab', 'ac']
-        self.assertEqual(grid.words, desired)
+        self.assertEqual(self.grid.words, desired)
 
     def test_placing_too_big_word(self):
-        grid = square.Grid(3)
         with self.assertRaises(AssertionError):
-            grid.place_word('verylong')
+            self.grid.place_word('verylongveryvery')
 
     def test_placing_word_vertical(self):
-        grid = square.Grid(5)
-        new_grid = grid.place_word('aaa', pos=(1, 0), direction=square.VERTICAL)
+        new_grid = self.grid.place_word('aaa', pos=(1, 0), direction=square.VERTICAL)
         # check that the old grid is not touched
-        self.assertEqual(grid[1][0], square.EMPTY)
+        self.assertEqual(self.grid[1][0], square.EMPTY)
         self.assertEqual(new_grid[1][0], 'a')
         self.assertEqual(new_grid[2][0], 'a')
         self.assertTrue(new_grid.is_valid(square.Words()))
 
     def test_placing_word_twice(self):
-        grid = square.Grid(5)
-        new_grid = grid.place_word('aaa', pos=(1, 0), direction=square.VERTICAL)
+        new_grid = self.grid.place_word('aaa', pos=(1, 0), direction=square.VERTICAL)
         with self.assertRaises(AssertionError):
             new_grid.place_word('bbb', (1, 0), direction=square.HORIZONTAL)
 
     def test_check_grid(self):
-        grid = square.Grid(3)
-        grid[0][0] = 'a'
-        grid[0][1] = 'b'
-        self.assertTrue(not grid.is_valid(square.Words()))
+        self.grid[0][0] = 'a'
+        self.grid[0][1] = 'b'
+        self.assertTrue(not self.grid.is_valid(square.Words()))
 
     def test_empty_cells(self):
-        grid = square.Grid(3)
-        gen = grid.empty_cells()
+        gen = self.grid.empty_cells()
         self.assertEqual(gen.next(), (0, 0))
 
     def test_string_list_to_grid(self):
